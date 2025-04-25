a1d67a7ee83a6f8e3d8fd03b5f1210e1cb85687d
Julia Lima <139915382+jumalisilva@users.noreply.github.com>
2025-04-25 01:03:36 +0000

    Step 4

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
@@ -5,3 +7,14 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    base_url = 'https://bookish-carnival-r4g7wq4q6999357r4-8000.app.github.dev/'
+    return Response({
+        'users': base_url + 'api/users/',
+        'teams': base_url + 'api/teams/',
+        'activities': base_url + 'api/activities/',
+        'leaderboard': base_url + 'api/leaderboard/',
+        'workouts': base_url + 'api/workouts/'
+    })
